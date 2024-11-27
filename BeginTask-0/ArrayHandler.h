bb1d425cfc0c23726079c42b821ab87454122023
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2024-11-27 16:37:43 +0300

    Update ArrayHandler.h

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,102 +1,65 @@
 #pragma once
 #include <iostream>
-#include <cmath>
+
+
 template<typename T>
-class ArrayHandler {
+class ArrayHandler
+{
 private:
-    size_t _size;
     T* _array;
-    size_t _count;// счетчик для добавления элемента чтобы не уйти за границы массива
+    size_t _size;//размер
+    size_t _count; //счетчик
 public:
-    ArrayHandler(size_t size) { 
-        _size = size;
+    ArrayHandler(){
+        _size = 10;
         _count = 0;
-        _array = new T{_size};
-    }
-
-    ArrayHandler() { 
-        _size = 5;
-        _count = 0;
-        _array = new T{_size};
-    }
-
-
-    void AppendElem(T elem) {
-        
-        if(_count == _size){ 
-            T* array;
-            array = new T[_size]; 
-            
-            for (size_t i = 0; i < _size; i++){
+        _array = new T[_size];
+    };
+    void AppendElem(T elem){
+        if (_count == _size){
+            T* array = new T[_size * _size]; //для копирования ниже
+            for(int i = 0; i < _size; i++){
                 array[i] = _array[i];
             }
+           
             delete [] _array;
-            _size++;
-            _array = new T{_size};
-            for (size_t i = 0; i < _size; i++){
-                _array[i] = array[i];
-            }
-            _array[_count] = elem;
-            delete [] array;
+            _size = _size * _size;//наперед увеличиваем
+            _array = array;
         }
-        else{
-            _array[_count] = elem;
-            _count++;
-        }
-    }
-    void pprint(){
-        for(int i = 0; i < _size; i++){
-            std::cout << _array[i] << " "; 
-        }
-        std::cout << std::endl;
-    }
+        
+        _array[_count++] = elem; //каунт увеличится после использования
 
-    bool IsContains(T elem) {
-        for(size_t i = 0; i < _size; i++){
-            if (_array[i] == elem){
-                std::cout<<"tuta"<< std::endl;
+    };
+    bool IsContains(const T& elem) const {
+        for(size_t i = 0; i < _count; i++){
+            if(elem == _array[i]){
                 return true;
             }
         }
-        std::cout<<" ne tuta" << std::endl;
         return false;
-        
-    }
-
-    T GetMax() {
-        T max;
-        max = _array[0];
-        for (size_t i = 1; i < _size; i++){
-            if (max < _array[i]){
+    };
+    T GetMax(){
+        T max = _array[0];
+        for(size_t i = 1; i < _count; i++)
+        {
+            if (_array[i] > max) {
                 max = _array[i];
             }
         }
-        std::cout << "max is " << max << std::endl;
         return max;
-    }
+    };
 
-    T GetMin() {
-        T min;
-        min = _array[0];
-        for (size_t i = 1; i < _size; i++){
-            if (min > _array[i]){
+    T GetMin(){
+        T min = _array[0];
+        for(size_t i = 1; i < _count; i++)
+        {
+            if (_array[i] < min) {
                 min = _array[i];
             }
         }
-        std::cout << "min is " << min << std::endl;
         return min;
-        
-    }
-    ArrayHandler& operator=(const ArrayHandler& tmp){
-        _size = tmp._size;
-        _count = tmp._count;
-        for (int i = 0; i < _size; i++){
-            _array[i] = tmp._array[i];
-        }
-        return *this;
-    }
-    ~ArrayHandler() {
+    };
+    ~ArrayHandler(){
         delete [] _array;
-    }
-
+    };
 };
